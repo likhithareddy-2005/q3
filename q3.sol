eb02e73790114bb348d464fc06d7244299349560
likhithareddy-2005 <132775262+likhithareddy-2005@users.noreply.github.com>
2023-06-10 15:08:58 +0530

    Update q3.sol

--- q3.sol
+++ q3.sol
@@ -1,31 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract q3{
-address payable public admin;
+contract CrowdfundingContract {
+ address payable public admin;
 uint public goal;
-uint public totalraised;
-uint totalamountraised;
+uint public deadline;
+ uint public totalAmountRaised;
 mapping(address => uint) public contributions;
-constructor(address payable _admin,uint _goal){
-admin=_admin;
-goal=_goal;
+constructor(address payable _admin, uint _goal, uint _days) {
+ admin = _admin;
+ goal = _goal;
+ deadline = block.timestamp + (_days * 1 days);
 }
- modifier onlyAdmin() {
-        require(msg.sender == admin, "Only the admin can perform this action.");
-        _;
-    }
-    function contribute() external payable  {
-        require(msg.value > 0, "Contribution amount must be greater than zero.");
-        totalamountraised += msg.value;
-        contributions[msg.sender] += msg.value;
-       
+modifier onlyAdmin() {
+ require(msg.sender == admin, "Only the admin can perform this action.");
+  _;
     }
 
-    function collectFunds() external onlyAdmin {
-        require(totalamountraised >= goal, "The funding goal has not been reached yet.");
-        
+ modifier beforeDeadline() {
+ require(block.timestamp < deadline, "The deadline has passed.");
+ _;
+ }
 
-        admin.transfer(address(this).balance);}
+ function contribute() external payable beforeDeadline {
+require(msg.value > 0, "Contribution amount must be greater than zero.");
+totalAmountRaised += msg.value;
+ contributions[msg.sender] += msg.value;
+    }
 
+function collectFunds() external onlyAdmin {
+ require(totalAmountRaised >= goal, "The funding goal has not been reached yet.");
+ require(block.timestamp >= deadline, "The deadline has not passed yet.");
+ admin.transfer(address(this).balance);
+   }
+ function refund() external beforeDeadline {
+require(address(this).balance > 0, "There are no funds to refund.");
+uint amountToRefund = contributions[msg.sender];
+contributions[msg.sender] = 0;
+ payable(msg.sender).transfer(amountToRefund);
+    }
+ 
 }
